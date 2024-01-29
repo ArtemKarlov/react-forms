859924e83633821f994b975120191fd74b8822eb
Artem Karlov <ar.karlov@gmail.com>
2024-01-30 00:46:54 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { UserConfig, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    base: "/",
+    server: {
+      host: "localhost",
+      port: 3000,
+    },
+    plugins: [react()],
+  };
+
+  if (command !== "serve") {
+    config.base = "/react-forms/";
+  }
+
+  return config;
+});
